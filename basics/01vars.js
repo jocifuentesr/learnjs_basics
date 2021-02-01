ab3faef6159d9670473bad87af614980e282ac3d
jocifuentesr <70120585+jocifuentesr@users.noreply.github.com>
2021-02-01 18:42:59 -0300

    math operators

---
+++ basics/01vars.js
@@ -0,0 +1,29 @@
+//Muestra por consola una variable primaria 
+// Recordar que el código se ejecuta de arriba hacia abajo
+// Variable numérica
+// js no distingue tipos de números
+let numero = 5;
+console.log(numero);
+
+// variable de tipo cadena
+// Se debe usar un tipo de comilla para abrir y cerrar. 
+let palabra = "Hola mundo";
+console.log(palabra);
+
+//variable booleana
+// true & false son palabras reservadas 
+let respuesta = true;
+console.log(respuesta);
+
+//Declara e inicia una constante
+// Las constantes no pueden cambiar su valor a través del código del programa
+const PI = 3.1415;
+console.log(PI);
+
+// Cambia el valor de la variable numero
+numero = -3.15;
+console.log(numero)
+
+// Cambia el valor de la variable palabra
+palabra = "Adiós";
+console.log(palabra);
---
+++ basics/02operators.js
@@ -0,0 +1,17 @@
+// Tres grupos de operadores
+// Matemáticos, Asignación, Incremento/Decremento
+
+// Operadores matemáticos
+/* let a = 7;
+let b = 2; */
+
+// operaciones básicas
+/* console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b); */
+
+// operación módulo
+/* console.log(a%b); */
+
+//Operadores de incremento/decremento
